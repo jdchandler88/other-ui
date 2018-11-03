0973cb0d831777598e230c458f48cacea981c33a
Jonathan Graham <jonathan.graham0628@gmail.com>
2018-11-02 22:50:31 -0400

    adding node_modules

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer v-model="drawer" fixed temporary app>
+      <v-list dense>
+        <v-subheader class="mt-3 grey--text text--darken-1">APPLICATIONS</v-subheader>
+        <v-list-tile v-for="item in items" :key="item.text" @click="launchApp">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title :ref="'app-' + item.text.toLowerCase().replace(' ', '-')">
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-subheader class="mt-3 grey--text text--darken-1">ADMINISTRATION</v-subheader>
+        <v-list>
+          <v-list-tile v-for="item in items2" :key="item.text" avatar>
+            <v-list-tile-avatar>
+              <img alt="N">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="item.text"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-list-tile class="mt-3">
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">add_circle_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Browse Channels</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="grey" dense fixed clipped-left app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-icon class="mx-3">important_devices</v-icon>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Application Title</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>person</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fill-height>
+        <!--<ApplicationWindow testc="TestApp"></ApplicationWindow>-->
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <v-tooltip right>
+              <v-btn
+                slot="activator"
+                :href="source"
+                icon
+                large
+                target="_blank"
+              >
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/YeRKwQ" target="_blank">
+                <v-icon large>mdi-codepen</v-icon>
+              </v-btn>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
+  data: () => ({
+    drawer: null,
+    items: [
+      { icon: "trending_up", text: "Most Popular" },
+      { icon: "subscriptions", text: "Subscriptions" },
+      { icon: "history", text: "History" },
+      { icon: "featured_play_list", text: "Playlists" },
+      { icon: "watch_later", text: "Watch Later" }
+    ],
+    items2: [
+      { picture: 28, text: "Joseph" },
+      { picture: 38, text: "Apple" },
+      { picture: 48, text: "Xbox Ahoy" },
+      { picture: 58, text: "Nokia" },
+      { picture: 78, text: "MKBHD" }
+    ]
+  }),
+  props: {
+    source: String
+  },
+  methods: {
+    launchApp: function() {
+      this.drawer = !this.drawer;
+      this.$panel.create({
+        theme: "primary",
+        headerTitle: "my panel #1",
+        position: "center-top 0 58",
+        contentSize: "450 250",
+        content: HelloWorld,
+        callback: function() {
+          this.content.style.padding = "20px";
+        },
+        onbeforeclose: function() {
+          return confirm("Do you really want to close the panel?");
+        }
+      });
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+//Import of basic components needed
 import Vue from "vue";
+import "./plugins/vuetify";
 import App from "./App.vue";
+import { jsPanel } from "jspanel4";
+
+//Import of CSS files required
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "jspanel4/es6module/jspanel.css";
 
 Vue.config.productionTip = false;
+
+Object.defineProperty(Vue.prototype, "$panel", { value: jsPanel });
 
 new Vue({
   render: h => h(App)
